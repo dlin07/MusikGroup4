67a3ded97cd4db104133890581ff29ff97e760f6
daMon <dl1023@scarletmail.rutgers.edu>
2024-04-13 18:33:06 -0400

    tried perspective transform

--- lib/visual/arucoDetection.py
+++ lib/visual/arucoDetection.py
@@ -4,7 +4,7 @@ import numpy as np
 import os as os
 
 markerPositions = []
-for i in range(0, 6):
+for i in range(0, 8):
     markerPositions.append((0, 0))
 # print(markerPositions)
 
@@ -14,7 +14,7 @@ detector = aruco.ArucoDetector(marker_dict, param_markers)
 
 cap = cv.VideoCapture(1)
 
-absolutePath = os.path.join(os.getcwd(), 'lib', 'visual', 'defaultAruco.png')
+absolutePath = os.path.join(os.getcwd(), 'lib', 'visual', 'arucoDisplay.png')
 
 calibration = cv.imread(absolutePath)
 calibrationR = cv.resize(calibration, (int(1920), int(1080)))
@@ -22,44 +22,46 @@ calibrationR = cv.resize(calibration, (int(1920), int(1080)))
 origImg = calibrationR
 
 cv.imshow('calibration', calibrationR)
-projectedTri = None
-realTri = None
+projectedPoints = None
+realPoints = None
 projectedToReal = None
 
 def transformCalibrationDisplay(mPos):
     global calibrationR
-    global projectedTri
-    global realTri
+    global projectedPoints
+    global realPoints
     global projectedToReal
-    # 0, 1, 2 = tl, tr, bl display triangle
-    # 3, 4, 5 = tl, tr, bl actual triangle
+    # 0, 1, 2, 3= tl, tr, br, bl projected quad
+    # 4, 5, 6, 7 = tl, tr, br, bl real quad
     # scale actual triangle to half the size
-    center = ((mPos[4][0] + mPos[5][0])/2, (mPos[4][1] + mPos[5][1])/2)
-    displayCenter = ((mPos[1][0] + mPos[2][0])/2, (mPos[1][1] + mPos[2][1])/2)
+    center = ((mPos[4][0] + mPos[6][0])/2, (mPos[4][1] + mPos[6][1])/2)
+    displayCenter = ((mPos[0][0] + mPos[2][0])/2, (mPos[0][1] + mPos[2][1])/2)
     
     offset1 = ((mPos[4][0] - center[0])/2, (mPos[4][1] - center[1])/2)
-    actualTrShrunk = (center[0] + offset1[0], center[1] + offset1[1])
+    actualTlShrunk = (center[0] + offset1[0], center[1] + offset1[1])
     offset2 = ((mPos[5][0] - center[0])/2, (mPos[5][1] - center[1])/2)
-    actualBlShrunk = (center[0] + offset2[0], center[1] + offset2[1])
-    offset3 = ((mPos[3][0] - center[0])/2, (mPos[3][1] - center[1])/2)
-    actualTlShrunk = (center[0] + offset3[0], center[1] + offset3[1])
+    actualTrShrunk = (center[0] + offset2[0], center[1] + offset2[1])
+    offset3 = ((mPos[6][0] - center[0])/2, (mPos[6][1] - center[1])/2)
+    actualBrShrunk = (center[0] + offset3[0], center[1] + offset3[1])
+    offset4 = ((mPos[7][0] - center[0])/2, (mPos[7][1] - center[1])/2)
+    actualBlShrunk = (center[0] + offset4[0], center[1] + offset4[1])
     
     print(mPos)
     # print(calibration.shape)
-    defaultTri = np.array([[480, 270], [1440, 270], [480, 810]]).astype(np.float32)
-    projectedTri = np.array([[mPos[0][0], mPos[0][1]],[mPos[1][0], mPos[1][1]],[mPos[2][0], mPos[2][1]]]).astype(np.float32)
-    realTri = np.array([[actualTlShrunk[0], actualTlShrunk[1]],[actualTrShrunk[0],actualTrShrunk[1]],[actualBlShrunk[0], actualBlShrunk[1]]]).astype(np.float32)
+    defaultPoints = np.array([[480, 270], [1440, 270], [1440, 810], [480, 810]]).astype(np.float32)
+    projectedPoints = np.array([[mPos[0][0], mPos[0][1]],[mPos[1][0], mPos[1][1]],[mPos[2][0], mPos[2][1]], [mPos[3][0], mPos[3][1]]]).astype(np.float32)
+    realPoints = np.array([[actualTlShrunk[0], actualTlShrunk[1]],[actualTrShrunk[0],actualTrShrunk[1]],[actualBrShrunk[0], actualBrShrunk[1]], [actualBlShrunk[0], actualBrShrunk[1]]]).astype(np.float32)
     # realTri = np.array([[mPos[3][0], mPos[3][1]],[mPos[4][0], mPos[4][1]],[mPos[5][0], mPos[5][1]]]).astype(np.float32)a 
 
     # print(projectedTri)
     # print(realTri)
-    defaultToCamera = cv.getAffineTransform(defaultTri, projectedTri)
-    cameraToDefault = cv.getAffineTransform(projectedTri, defaultTri)
-    projectedToReal = cv.getAffineTransform(projectedTri, realTri)
+    defaultToCamera = cv.getPerspectiveTransform(defaultPoints, projectedPoints)
+    cameraToDefault = cv.getPerspectiveTransform(projectedPoints, defaultPoints)
+    projectedToReal = cv.getPerspectiveTransform(projectedPoints, realPoints)
 
-    warpedImg = cv.warpAffine(calibrationR, defaultToCamera, (calibrationR.shape[1], calibrationR.shape[0]))
-    warpedImg = cv.warpAffine(warpedImg, projectedToReal, (warpedImg.shape[1], warpedImg.shape[0]))
-    warpedImg = cv.warpAffine(warpedImg, cameraToDefault, (warpedImg.shape[1], warpedImg.shape[0]))
+    warpedImg = cv.warpPerspective(calibrationR, defaultToCamera, (calibrationR.shape[1], calibrationR.shape[0]))
+    warpedImg = cv.warpPerspective(warpedImg, projectedToReal, (warpedImg.shape[1], warpedImg.shape[0]))
+    warpedImg = cv.warpPerspective(warpedImg, cameraToDefault, (warpedImg.shape[1], warpedImg.shape[0]))
 
     # translate
     # translation_matrix = np.float32([ [1,0,center[0] - displayCenter[0]], [0,1,center[1] - displayCenter[1]] ])   
@@ -78,18 +80,18 @@ while True:
     marker_corners, marker_IDs, reject = detector.detectMarkers(gray_frame)
     # print(marker_corners)
     
-    if not (projectedTri is None):
-        cv.polylines(
-            frame, [projectedTri.astype(np.int32)], True, (0,255,255)
-        )
-        cv.polylines(
-            frame, [realTri.astype(np.int32)], True, (0,255,255)
-        )
+    # if not (projectedPoints is None):
+    #     cv.polylines(
+    #         frame, [projectedPoints.astype(np.int32)], True, (0,255,255)
+    #     )
+    #     cv.polylines(
+    #         frame, [realPoints.astype(np.int32)], True, (0,255,255)
+    #     )
         # display the warped triangle and see if it matches
-        transformed = cv.transform(np.array([projectedTri]), projectedToReal)
-        cv.polylines(
-            frame, [transformed.astype(np.int32)], True, (255,0,0)
-        )
+        # transformed = cv.transform(np.array([projectedPoints]), projectedToReal)
+        # cv.polylines(
+        #     frame, [transformed.astype(np.int32)], True, (255,0,0)
+        # )
 
     if marker_corners:
         for ids, corners in zip(marker_IDs, marker_corners):
@@ -117,7 +119,7 @@ while True:
             for i in range(0, 4):
                 curPos = (curPos[0] + corners[i][0], curPos[1] + corners[i][1])
             curPos = (curPos[0] / 4, curPos[1] / 4)
-            if (1 <= ids[0] and ids[0] <= 6) :
+            if (1 <= ids[0] and ids[0] <= 8) :
                 markerPositions[ids[0]-1] = curPos
             
     cv.imshow("frame", frame)
